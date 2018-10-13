97c95781554a358d33589fcfbe072bda5c001104
runtu <huorunquan@126.com>
2018-10-13 19:19:17 +0800

    IMP: 更新目录

--- editor/src/pages/.umi/DvaContainer.js
+++ editor/src/pages/.umi/DvaContainer.js
@@ -4,17 +4,17 @@ import createLoading from 'dva-loading';
 
 let app = dva({
   history: window.g_history,
-  ...((require('D:/code/cmrh/cms_all/cms_all_in_one/editor/src/dva.js').config || (() => ({})))()),
+  ...((require('D:/code/mine/cms_all_in_one/editor/src/dva.js').config || (() => ({})))()),
 });
 
 window.g_app = app;
 app.use(createLoading());
 
-app.model({ namespace: 'model', ...(require('D:/code/cmrh/cms_all/cms_all_in_one/editor/src/pages/site/model.js').default) });
-app.model({ namespace: 'model', ...(require('D:/code/cmrh/cms_all/cms_all_in_one/editor/src/pages/component/model.js').default) });
-app.model({ namespace: 'model', ...(require('D:/code/cmrh/cms_all/cms_all_in_one/editor/src/pages/editor/$id/model.js').default) });
-app.model({ namespace: 'model', ...(require('D:/code/cmrh/cms_all/cms_all_in_one/editor/src/pages/site/$id/model.js').default) });
-app.model({ namespace: 'model', ...(require('D:/code/cmrh/cms_all/cms_all_in_one/editor/src/pages/template/model.js').default) });
+app.model({ namespace: 'model', ...(require('D:/code/mine/cms_all_in_one/editor/src/pages/site/model.js').default) });
+app.model({ namespace: 'model', ...(require('D:/code/mine/cms_all_in_one/editor/src/pages/component/model.js').default) });
+app.model({ namespace: 'model', ...(require('D:/code/mine/cms_all_in_one/editor/src/pages/editor/$id/model.js').default) });
+app.model({ namespace: 'model', ...(require('D:/code/mine/cms_all_in_one/editor/src/pages/site/$id/model.js').default) });
+app.model({ namespace: 'model', ...(require('D:/code/mine/cms_all_in_one/editor/src/pages/template/model.js').default) });
 
 class DvaContainer extends Component {
   render() {
